dfa0c6da3183c4be64fd8027f9b52f92a4d73bed
Campbell <51032030+20acgarvi@users.noreply.github.com>
2019-08-16 10:52:20 -0500

    Update TkCustom.py
    Added automated image additions and upload button

--- TkCustom.py
+++ TkCustom.py
@@ -17,14 +17,12 @@ class AddPopup(Tk):
         #intializes the frame
         self.canvas = Canvas(self)
         self.canvas.pack(expand = 1, fill = "both")
-        #add images
-        try:
-            self.image = ImageTk.PhotoImage(Image.open(
-                "/Users/arthurgarvin/Downloads/chicken-man-seed.jpg"
-                ))
-            self.canvas.create_image((20,20), image = self.image, anchor = NW)
-        except FileNotFoundError:
-            pass
+
+        self.images = []
+        self.createimage((20,20),
+                image="/Users/arthurgarvin/Downloads/chicken-man-seed.jpg")
+
+        #adds rectangles
         self.canvas.create_rectangle(210, 20, 250, 60, fill = "")
         self.canvas.create_rectangle(260, 20, 300, 60, fill = "")
         self.canvas.create_rectangle(210, 70, 250, 110, fill = "")
@@ -37,14 +35,35 @@ class AddPopup(Tk):
         self.label.place(x = 310, y = 20)
         self.name_entry = Entry(self)
         self.name_entry.place(x = 360, y = 20)
-        self.button = Button(self, text = "Upload Image")
+        self.button = Button(self, text = "Upload Image", command = self.upload)
         self.button.place(x = 60, y = 210)
         self.button2 = Button(self, text = "Add Scenario")
         self.button2.place(x = 460, y = 310)
 
     def upload(self):
         filename = filedialog.askopenfilename()
-        #
+        self.createimage((20,20), image = filename, index = 0)
+
+    def createimage(self, *args, **kwargs):
+        t = (0,0)
+        for argv in args:
+            if isinstance(argv, tuple):
+                t = argv
+        i = ""
+        if "image" in kwargs:
+            i = kwargs["image"]
+        #add images
+        try:
+            tki = ImageTk.PhotoImage(Image.open(i))
+            self.canvas.create_image(t, image = tki, anchor = NW)
+        except FileNotFoundError:
+            pass
+        if "index" in kwargs:
+            n = kwargs["index"]
+            self.images[n] = tki
+        else:
+            self.images.append(tki)
+        
 
 if __name__ == "__main__":
     root = AddPopup()
